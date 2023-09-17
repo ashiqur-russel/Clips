94a7cdce1d528f3b9fc4ad83fa58e0d4502d2612
Ashiqur <ashiqur.rahman@bidi.one>
2023-09-17 22:33:47 +0200

    Refactor modal service and load dynamic modal using modal id

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/service/modal.service';
 
 @Component({
@@ -8,7 +8,7 @@ import { ModalService } from 'src/app/service/modal.service';
 })
 export class ModalComponent implements OnInit {
 
-
+@Input() modalID = ''
   constructor(public modal: ModalService){
   }
 
@@ -16,7 +16,7 @@ export class ModalComponent implements OnInit {
     
   }
   onCloseModal(){
-    this.modal.toggleModal()
+    this.modal.toggleModal(this.modalID)
   }
 
 }
--- src/app/user/auth-modal/auth-modal.component.ts
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/service/modal.service';
 
 @Component({
   selector: 'app-auth-modal',
   templateUrl: './auth-modal.component.html',
   styleUrls: ['./auth-modal.component.scss']
 })
-export class AuthModalComponent {
+export class AuthModalComponent implements OnInit {
 
   showLogin:boolean = true;
 
+  constructor( public modal: ModalService){}
 
+ngOnInit(): void {
+  this.modal.register('auth');
+  this.modal.register('test')
+}
   showLoginForm(){
     this.showLogin = true;
   }
