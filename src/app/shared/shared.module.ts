292ca5743712584bace25e7a53b75d51547bac6d
Ashiqur <ashiqur.rahman@bidi.one>
2023-10-09 23:33:33 +0200

    input component created

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { ModalComponent } from './modal/modal.component';
 import { ModalService } from '../service/modal.service';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabsComponent } from './tabs/tabs.component';
+import { InputComponent } from './input/input.component';
 
 
 
@@ -11,7 +12,8 @@ import { TabsComponent } from './tabs/tabs.component';
   declarations: [
     ModalComponent,
     TabsContainerComponent,
-    TabsComponent
+    TabsComponent,
+    InputComponent
   ],
   imports: [
     CommonModule
